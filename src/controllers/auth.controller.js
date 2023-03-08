4f8ab500d6dbfe6c9e7aa82e61d7a3d39f9f6bd2
lucasmartins-dev <lucasmartinsms.dev@gmail.com>
2023-03-08 19:08:13 -0300

    signUp connection

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,5 @@
 import bcrypt from "bcrypt"
-import db from "../config/database.connection.js";
+import {db} from "../config/database.connection.js";
 import { v4 as uuid } from "uuid";
 
 export async function signUp(req, res) {
@@ -22,4 +22,30 @@ try {
     res.status(500).send(err.message)
   }
 
+}
+
+export async function signIn(req, res) {
+  const { email, password } = req.body
+
+  try {
+    const user = await db.query('SELECT * FROM users WHERE email = $1', [email])
+
+    if (user.rowCount === 0) return res.sendStatus(401)
+
+    if (!bcrypt.compareSync(password, user.rows[0].password)) return res.sendStatus(401)
+
+    const userSession = await db.query('SELECT * FROM session WHERE "userId" = $1', [user.rows[0].id])
+
+    const token = uuid()
+
+    if (userSession.rowCount !== 0) {
+      await db.query('UPDATE session SET token = $1', [token])
+    }else {
+      await db.query('INSERT INTO session (token, "userId") VALUES ($1, $2)', [token, user.rows[0].id])
+    }
+
+    res.send({ token }) 
+  } catch (err) {
+    res.status(500).send(err.message)
+  }
 }

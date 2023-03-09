a48cb5d71d7e316d63019220506215e803814ff9
Lucca Viccini <luccaviccini@gmail.com>
2023-03-09 16:28:38 -0300

    feat: using url-metadata to get data from url posted by user

--- src/controllers/timeline.controllers.js
+++ src/controllers/timeline.controllers.js
@@ -1,36 +1,51 @@
-import {db} from "../config/database.connection.js";
-
+import { db } from "../config/database.connection.js";
+import urlMetadata from "url-metadata";
 
 export async function postNewPost(req, res) {
-    // get user id from session
-    const { userId } = res.locals.session;
-    // get url and description from req.body
-    const { url, description } = req.body;
-    // insert into database
-    try{
-        await db.query(`
+  // get user id from session
+  const { userId } = res.locals.session;
+  // get url and description from req.body
+  const { url, description } = req.body;
+  // insert into database
+  try {
+    await db.query(
+      `
             INSERT INTO posts ("userId", url, description) 
             VALUES ($1, $2, $3)
-        `, [userId, url, description]);
-        res.sendStatus(201);       
-
-    }catch(error){
-        console.error(error);
-        res.status(500).send("Internal server error");
-    }   
+        `,
+      [userId, url, description]
+    );
+    res.sendStatus(201);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal server error");
+  }
 }
 
 export async function getPosts(req, res) {
-    try{
-        const posts = await db.query(`
+  try {
+    const posts = await db.query(`
             SELECT * FROM posts
             ORDER BY id DESC
             LIMIT 20
         `);
-        res.send(posts.rows);
-    }catch(error){
-        console.error(error);
-        res.status(500).send("Internal server error");
-    }
-}
 
+    const postData = await Promise.all(
+      posts.rows.map(async (post) => {
+        const metadata = await urlMetadata(post.url);
+        return {
+          title: metadata.title,
+          description: metadata.description,
+          imageUrl: metadata.image,
+          siteUrl: metadata.url,
+        };
+      })
+    );
+
+    
+    res.send(postData);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal server error");
+  }
+}
